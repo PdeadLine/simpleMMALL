94ccb619972600f1dbbee896c080601c71edcbc2
pdeadline <pdeadline@163.com>
2018-04-13 16:33:53 +0800

    upload

--- src/main/java/com/tmall/controller/backend/ProductManageController.java
+++ src/main/java/com/tmall/controller/backend/ProductManageController.java
@@ -5,18 +5,26 @@ import com.tmall.common.ResponseCode;
 import com.tmall.common.ServerResponse;
 import com.tmall.pojo.Product;
 import com.tmall.pojo.User;
+import com.tmall.service.IFileService;
 import com.tmall.service.IProductService;
 import com.tmall.service.IUserService;
+import com.tmall.util.PropertiesUtil;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -30,6 +38,8 @@ public class ProductManageController {
     private IUserService iUserService;
     @Autowired
     private IProductService iProductService;
+    @Autowired
+    private IFileService iFileService;
 
     /**
      * 新增产品或更新产品
@@ -94,6 +104,12 @@ public class ProductManageController {
         return ServerResponse.createByErrorMessage("非管理员用户，无权限操作");
     }
 
+    /**
+     * 商品详细
+     * @param productId
+     * @param session
+     * @return
+     */
     @ResponseBody
     @RequestMapping("detial.do")
     public ServerResponse getDetial(Integer productId, HttpSession session) {
@@ -128,9 +144,78 @@ public class ProductManageController {
         return ServerResponse.createByErrorMessage("非管理员用户，无权限操作");
     }
 
-    public ServerResponse upload(MultipartFile file, HttpServletRequest request) {
-        String path = request.getSession().getServletContext().getRealPath("upload");
-        return null;
+    /**
+     * 上传文件
+     * @param session
+     * @param file
+     * @param request
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("upload.do")
+    public ServerResponse upload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        if (iUserService.checkAdminRoll(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file, path);
+            String url= PropertiesUtil.getProperty("")+targetFileName;
+
+            Map fileMap = new HashMap();
+            fileMap.put("uri",targetFileName );
+            fileMap.put("url",url);
+            return ServerResponse.createBySuccessData(fileMap);//返回一个图片的URL地址
+        }
+        return ServerResponse.createByErrorMessage("非管理员用户，无权限操作");
     }
 
+    /**
+     *  富文本图片上传
+     * @param session
+     * @param file
+     * @param request
+     * @param response
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("richtext_img_upload.do")
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file,
+                                 HttpServletRequest request, HttpServletResponse response){
+        Map resultMap = new HashedMap();
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "请登陆管理员");
+            return resultMap;
+        }
+        if (iUserService.checkAdminRoll(user).isSuccess()) {
+            //富文本中对于返回值有自己的要求,我们使用是simditor所以按照simditor的要求进行返回
+//        {
+//            "success": true/false,
+//                "msg": "error message", # optional
+//            "file_path": "[real file path]"
+//        }
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file, path);
+            if (StringUtils.isBlank(targetFileName)) {
+                resultMap.put("success", false);
+                resultMap.put("msg", "上传失败");
+                return resultMap;
+            }
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+            resultMap.put("success", true);
+            resultMap.put("msg", "上传成功");
+            resultMap.put("file_path", url);
+            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            return resultMap;
+
+        } else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "无权限操作");
+            return resultMap;
+        }
+
+    }
 }
--- src/main/java/com/tmall/service/IFileService.java
+++ src/main/java/com/tmall/service/IFileService.java
@@ -1,8 +1,12 @@
 package com.tmall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * create by sintai
  */
 
 public interface IFileService {
+    public String upload(MultipartFile file, String path);
+
 }
