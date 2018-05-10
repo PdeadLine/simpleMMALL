dd4d320b68547ac94c70a3559313a9427ca58dbb
pdeadline <pdeadline@163.com>
2018-05-10 22:51:54 +0800

    validate callBack is right

--- src/main/java/com/tmall/controller/portal/OrderController.java
+++ src/main/java/com/tmall/controller/portal/OrderController.java
@@ -1,16 +1,25 @@
 package com.tmall.controller.portal;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
+import com.google.common.collect.Maps;
 import com.tmall.common.Const;
 import com.tmall.common.ResponseCode;
 import com.tmall.common.ServerResponse;
 import com.tmall.pojo.User;
 import com.tmall.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * create by sintai
@@ -18,6 +27,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/order/")
 public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private IOrderService iOrderService;
@@ -28,6 +38,8 @@ public class OrderController {
      * @param request
      * @return
      */
+    @ResponseBody
+    @RequestMapping("pay.do")
     public ServerResponse pay(HttpSession session, long orderNo, HttpServletRequest request) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null) {
@@ -36,4 +48,37 @@ public class OrderController {
         String path = request.getSession().getServletContext().getRealPath("upload");
         return iOrderService.pay(user.getId(),orderNo,path);
     }
+
+    @ResponseBody
+    @RequestMapping("alipay_callback.do")
+    public Object alipayCallBack(HttpServletRequest request) {  //支付宝的回调所有的数据都放在request中
+        Map<String,String> params= Maps.newHashMap();   //取得request中的数据，拼接，放到map中
+
+
+        Map requestParams=request.getParameterMap();
+        //这里使用迭代器iterator  --技巧---map的迭代
+        for (Iterator iter=requestParams.keySet().iterator();iter.hasNext()) {
+            String name =(String)iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                //技巧又来了---简化了很多代码
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i];
+            }
+            params.put(name, valueStr);
+        }
+        logger.info("支付宝回调，sign:{},trade_status：{}，参数：{}",params.get("sign"),params.get("trade_status"),params.toString());
+        //非常重要，验证回调的正确性，是不是支付宝发的，并且还要避免重复通知。
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(), "utf-8", Configs.getSignType());
+            if (!alipayRSACheckedV2) {
+                return ServerResponse.createByErrorMessage("非法请求，验证未通过，再恶意请求就关闭你的电脑！！！");
+            }
+        } catch (AlipayApiException e) {
+           logger.error("支付宝验证回调一场",e);
+        }
+        //todo 验证各种数据
+    }
+
 }
